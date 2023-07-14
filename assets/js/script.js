e9fd6e9a8f92f2f295a169590c98026ba7bbcf39
mchung03 <minhee.chung14@gmail.com>
2023-07-13 21:05:34 -0700

    temp, wind, humidity text done

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,26 +48,25 @@ var getWeather = function(latitude, longitude) {
 
 var renderForecastData = function(selectedData) {
     var card1Div = document.querySelector("#card1")
-    card1Div.textContent = selectedData[0].main.temp
-    // card1Div.textContent = selectedData[0].wind.speed
-    // card1Div.textContent = selectedData[0].main.humidity
+    card1Div.textContent = "Temp: " + selectedData[0].main.temp + "°F\n"
+    card1Div.textContent += "Wind: " + selectedData[0].wind.speed + "mph\n"
+    card1Div.textContent += "Humidity: " + selectedData[0].main.humidity + "%\n"
     var card2Div = document.querySelector("#card2")
-    card2Div.textContent = selectedData[1].main.temp
-    // card2Div.textContent = selectedData[1].wind.speed
-    // card2Div.textContent = selectedData[1].main.humidity
+    card2Div.textContent = "Temp: " + selectedData[1].main.temp + "°F\n"
+    card2Div.textContent += "Wind: " + selectedData[1].wind.speed + "mph\n"
+    card2Div.textContent += "Humidity: " + selectedData[1].main.humidity + "%\n"
     var card3Div = document.querySelector("#card3")
-    card3Div.textContent = selectedData[2].main.temp
-    // card3Div.textContent = selectedData[2].wind.speed
-    // card3Div.textContent = selectedData[2].main.humidity
+    card3Div.textContent = "Temp: " + selectedData[2].main.temp + "°F\n"
+    card3Div.textContent += "Wind: " + selectedData[2].wind.speed + "mph\n"
+    card3Div.textContent += "Humidity: " + selectedData[2].main.humidity + "%\n"
     var card4Div = document.querySelector("#card4")
-    card4Div.textContent = selectedData[3].main.temp
-    // card4Div.textContent = selectedData[3].wind.speed
-    // card4Div.textContent = selectedData[3].main.humidity
+    card4Div.textContent = "Temp: " + selectedData[3].main.temp + "°F\n"
+    card4Div.textContent += "Wind: " + selectedData[3].wind.speed + "mph\n"
+    card4Div.textContent += "Humidity: " + selectedData[3].main.humidity + "%\n"
     var card5Div = document.querySelector("#card5")
-    card5Div.textContent = selectedData[4].main.temp
-    // card5Div.textContent = selectedData[4].wind.speed
-    // card5Div.textContent = selectedData[4].main.humidity
-
+    card5Div.textContent = "Temp: " + selectedData[4].main.temp + "°F\n"
+    card5Div.textContent += "Wind: " + selectedData[4].wind.speed + "mph\n"
+    card5Div.textContent += "Humidity: " + selectedData[4].main.humidity + "%\n"
 }
 
 var getCurrentWeather = function(latitude, longitude) {
@@ -86,14 +85,14 @@ var getCurrentWeather = function(latitude, longitude) {
 var renderCurrentData = function(data) {
     var cityName = document.querySelector('#city')
     cityName.textContent = data.name
-    var todaysDate = document.querySelector('#date')
+    // var todaysDate = document.querySelector('#date')
+    // todaysDate.textContent = dayjs()
     var currentTemp = document.querySelector('#temp')
-    currentTemp.textContent = data.main.temp
+    currentTemp.textContent = "Temp: " + data.main.temp + "°F"
     var currentWind = document.querySelector('#wind')
-    currentWind.textContent = data.wind.speed
+    currentWind.textContent = "Wind: " + data.wind.speed + "mph"
     var currentHum = document.querySelector('#humidity')
-    currentHum.textContent = data.main.humidity
+    currentHum.textContent = "Humidity: " + data.main.humidity + "%"
 }
 
-// searchBtn.addEventListener('click', getWeather);
 cityFormEl.addEventListener("submit", formSubmitHandler)
